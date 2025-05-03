4001b7fabb9016cab5346dbe036c3ed19f425907
koushik <191687017+koushik255@users.noreply.github.com>
2025-05-03 18:49:55 -0400

    fnishcd chan

--- advance/chanlsync/main.go
+++ advance/chanlsync/main.go
@@ -3,7 +3,10 @@ package main
 //ensures that datta is properly exchaneg between
 // goroutines
 // import "time"
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // func main() {
 // 	done := make(chan bool)
@@ -21,14 +24,65 @@ import "fmt"
 
 // }
 
+// func main() {
+// 	ch := make(chan int)
+
+// 	go func() {
+// 		ch <- 9 // Blocking until the value is received
+// 		fmt.Println("Sent value:")
+// 	}()
+// 	value := <-ch //blocking until a value is sent
+// 	fmt.Println(value)
+// }
+
+// SYNCHRONIZING MULTIPEL GO ROUTINES
+// and ensyring that all goroutines are finished
+// func main() {
+// 	numGoroutines := 3
+// 	done := make(chan int, 3)
+
+// 	for i := range numGoroutines {
+// 		// each time the loop runs the go keyword
+// 		// launches a new independnt routine
+// 		time.Sleep(2 * time.Second)
+// 		go func(id int) {
+// 			fmt.Printf("Goroutine %d working\n", id)
+// 			done <- id // signal to confirm its finished
+// 		}(i)
+// 	}
+
+// 	for range 2 {
+// 		<-done // Waiting for each each gorutine to finsih
+// 	}
+
+// 	fmt.Println("ALl goroutines are finished")
+// }
+
+// SYNCHRONIZING DATA EXCHANGE
 func main() {
-	ch := make(chan int)
+	//using channels to pass data around
+	data := make(chan string)
 
 	go func() {
-		ch <- 9
-		fmt.Println("Sent value:")
-
-		value := <-ch
-		fmt.Println(value)
+		for i := range 5 {
+			data <- "Hello :" + string('0'+i)
+			time.Sleep(100 * time.Millisecond)
+		} // simlutating that we are sneidng data 5 times
+		close(data) // close the go routine once there is no more
+		//data to send
 	}()
+	// using channels to send data in a sequencial way
+
+	// since its a continuous flow of data we must use a for loop
+	// to receive all the data
+
+	// this channel is still open so that our for loop is still
+	// receiving values, it creates a receiver for something which
+	// has noo sneder so then it calls deadlock
+
+	for value := range data {
+		fmt.Println("Recvied date:", value, time.Now())
+	} // alternate way to receive data from a flow of data from
+	// channel
+
 }
